d444f4d5787de2d5967bc28617ff5a4f7a0f3622
Ismael <ismaelJimenez@users.noreply.github.com>
2017-06-18 17:11:04 +0200

    Create geth-installer.sh

---
+++ geth-installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Source: https://github.com/EthEmbedded/Raspi-Eth-Install/blob/master/geth-installer.sh
+
+#Upgrade Debian to "stretch"
+echo -e "\e[32mFirst we need to edit our sources.list to set stretch/testing as our source so we can install latest versions of dependencies\e[0m"
+sudo sed -i /deb/s/wheezy/stretch/g /etc/apt/sources.list
+sudo sed -i /deb/s/jessie/stretch/g /etc/apt/sources.list
+echo -e "\e[34mPerforming Housekeeping - Update...\e[0m"
+sudo apt-get -y update
+echo -e "\e[34mPerforming Housekeeping - Upgrade...\e[0m"
+sudo apt-get -y upgrade
+echo -e "\e[34mPerforming Debian Upgrade from `Jessie` to `Stretch`...\e[0m"
+sudo apt-get -y dist-upgrade 
+
+#Install dependencies
+echo -e "\e[34mInstalling Dependencies...\e[0m"
+sudo apt-get -y install dphys-swapfile build-essential libgmp3-dev golang git python curl
+
+#Install go-ethereum OR `geth`
+echo -e "\e[34mInstalling go-ethereum...\e[0m"
+cd ~
+git clone https://github.com/ethereum/go-ethereum
+cd go-ethereum
+make geth
