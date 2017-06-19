05644fce704ac84835e14f8f14d325f7011164d2
Ismael <ismaelJimenez@users.noreply.github.com>
2017-06-19 20:46:16 +0200

    Update geth-installer.sh

--- geth-installer.sh
+++ geth-installer.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# Source: https://github.com/EthEmbedded/Raspi-Eth-Install/blob/master/geth-installer.sh
-
-#Upgrade Debian to "stretch"
-echo -e "\e[32mFirst we need to edit our sources.list to set stretch/testing as our source so we can install latest versions of dependencies\e[0m"
-sudo sed -i /deb/s/wheezy/stretch/g /etc/apt/sources.list
-sudo sed -i /deb/s/jessie/stretch/g /etc/apt/sources.list
+#Upgrade Debian
 echo -e "\e[34mPerforming Housekeeping - Update...\e[0m"
 sudo apt-get -y update
 echo -e "\e[34mPerforming Housekeeping - Upgrade...\e[0m"
-sudo apt-get -y upgrade
-echo -e "\e[34mPerforming Debian Upgrade from `Jessie` to `Stretch`...\e[0m"
-sudo apt-get -y dist-upgrade 
+sudo apt-get -y upgrade 
 
-#Install dependencies
-echo -e "\e[34mInstalling Dependencies...\e[0m"
-sudo apt-get -y install dphys-swapfile build-essential libgmp3-dev golang git python curl
-sudo apt --fix-broken install
-sudo apt-get install golang
-
-#Install go-ethereum OR `geth`
-echo -e "\e[34mInstalling go-ethereum...\e[0m"
+#Install geth
+echo -e "\e[34mInstalling geth...\e[0m"
 cd ~
-git clone https://github.com/ethereum/go-ethereum
-cd go-ethereum
-make geth
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm7-1.6.5-cf87713d.tar.gz
+tar -xzvf geth-linux-arm7-1.6.5-cf87713d.tar.gz
+sudo cp geth-linux-arm7-1.6.5-cf87713d/geth /usr/local/bin
