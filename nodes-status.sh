71b26ccacff53a0dcb83e7679f18fa00c8927152
Ismael <ismaelJimenez@users.noreply.github.com>
2017-06-19 22:48:03 +0200

    Update nodes-status.sh

--- nodes-status.sh
+++ nodes-status.sh
@@ -5,6 +5,6 @@ location=/media/pi/My_Files
 #Start bitcoin
 echo -e "\e[34mBitcoin latest block...\e[0m"
 bitcoin-cli -datadir=$location/bitcoin getblockcount
-echo -e "\e[Ethereum latest action...\e[0m"
+echo -e "\e[34mEthereum latest action...\e[0m"
 tail -1 ~/geth-log
 
