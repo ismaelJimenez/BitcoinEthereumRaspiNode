b37c9cca0c462dea9aabe6a3b4e0ad04771b7e9c
Ismael <ismaelJimenez@users.noreply.github.com>
2017-06-19 20:47:09 +0200

    Update bitcoin-installer.sh

--- bitcoin-installer.sh
+++ bitcoin-installer.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#Upgrade Debian
+echo -e "\e[34mPerforming Housekeeping - Update...\e[0m"
+sudo apt-get -y update
+echo -e "\e[34mPerforming Housekeeping - Upgrade...\e[0m"
+sudo apt-get -y upgrade 
+
+#Install bitcoin
+echo -e "\e[34mInstalling bitcoin...\e[0m"
+cd ~
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm7-1.6.5-cf87713d.tar.gz
+tar -xzvf geth-linux-arm7-1.6.5-cf87713d.tar.gz
+sudo cp geth-linux-arm7-1.6.5-cf87713d/geth /usr/local/bin
+
+
+
+
 # To be executed after ethereum-installer.sh!
 
 #Install dependencies
