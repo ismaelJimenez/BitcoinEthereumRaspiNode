1443840213aaab7248f18aeca543e72355e3c56c
Ismael <ismaelJimenez@users.noreply.github.com>
2017-06-19 20:55:11 +0200

    Update bitcoin-installer.sh

--- bitcoin-installer.sh
+++ bitcoin-installer.sh
@@ -9,37 +9,8 @@ sudo apt-get -y upgrade
 #Install bitcoin
 echo -e "\e[34mInstalling bitcoin...\e[0m"
 cd ~
-wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm7-1.6.5-cf87713d.tar.gz
-tar -xzvf geth-linux-arm7-1.6.5-cf87713d.tar.gz
-sudo cp geth-linux-arm7-1.6.5-cf87713d/geth /usr/local/bin
-
-
-
-
-# To be executed after ethereum-installer.sh!
-
-#Install dependencies
-echo -e "\e[34mInstalling Dependencies...\e[0m"
-sudo apt-get -y install autoconf libevent-dev libtool libssl-dev libboost-all-dev libminiupnpc-dev
-sudo apt-get -y install qt4-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
-
-mkdir ~/bin
-cd ~/bin
-
-# Download the Berkeley database source code
-wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-tar -xzvf db-4.8.30.NC.tar.gz
-cd db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx
-make -j4
-sudo make install
-
-#Install go-ethereum OR `geth`
-echo -e "\e[34mInstalling bitcoin...\e[0m"
-cd ~/
-git clone -b 0.14 https://github.com/bitcoin/bitcoin.git
-cd bitcoin/
-./autogen.sh
-./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default --with-gui
-make -j2
-sudo make install
+wget https://bitcoin.org/bin/bitcoin-core-0.14.2/bitcoin-0.14.2-arm-linux-gnueabihf.tar.gz
+tar -xzvf bitcoin-0.14.2-arm-linux-gnueabihf.tar.gz
+sudo cp bitcoin-0.14.2/bin/* /usr/local/bin
+rm bitcoin-0.14.2-arm-linux-gnueabihf.tar.gz
+rm -rf bitcoin-0.14.2
